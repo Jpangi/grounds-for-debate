204b6ff64cc88ccf78f69d7e1168a0b964be7c60
jpangi <jacob.Pangilinan10@gmail.com>
2025-12-12 11:23:44 -0800

    created the sign-up and login controller functions

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// =======================
+// ======= IMPORTS =======
+// =======================
+const dotenv = require("dotenv");
+dotenv.config(); //code that allows env variables
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express(); // the express framework is in the app variable
+
+// connection to the database code
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
+let PORT = process.env.PORT;
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
