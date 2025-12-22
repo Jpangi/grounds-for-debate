dd5190af8b5f0c91b27246ffd9ad63b6537a9f1e
jpangi <jacob.Pangilinan10@gmail.com>
2025-12-22 13:00:57 -0800

    changed the edit component so that it shows the default values in the edit form

--- client/src/components/coffee/EditCoffee.jsx
+++ client/src/components/coffee/EditCoffee.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useNavigate, useParams } from "react-router";
 import { BASEURL } from "../../services/contants";
+import { useState, useEffect } from "react";
 
 
 const EditCoffee = (props) => {
@@ -47,6 +48,34 @@ const navigate = useNavigate();
   const roast = ["Light", "Medium", "Medium-Dark", "Dark"];
   const rating = [1, 2, 3, 4, 5];
 
+
+
+  const [singleCoffee, setSingleCoffee] = useState(null);
+
+   useEffect(() => {
+        const fetchCoffee = async () => {
+            try {
+                const res = await axios.get(`${BASEURL}coffee/${coffeeId}`, {
+                    headers: {
+                        Authorization: `Bearer ${props.user}`,
+                    },
+                });
+                setSingleCoffee(res.data);
+            } catch (error) {
+                console.log("Error fetching coffee:", error);
+            }
+        };
+        
+        if (coffeeId) {
+            fetchCoffee();
+        }
+    }, [coffeeId]);
+
+
+
+
+
+
   const handleEdit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target); //put all the form data in the variable called 'formData'
@@ -74,15 +103,15 @@ const navigate = useNavigate();
       console.log(`handleEdit Error:`, error);
     }
   };
-
+  if(!singleCoffee) return <div>Loading....</div>
   return (
     <>
       <section>
-        <h2>Edit Coffee Beans</h2>
+        <h2>Edit: {singleCoffee.name}</h2>
         <form onSubmit={handleEdit}>
           <label htmlFor="name">Roaster: </label>
-          <select name="name" required>
-            <option value="">Select Coffee Roaster</option>
+          <select name="name" required defaultValue={singleCoffee.name}>
+            <option >Select Coffee Roaster</option>
             {coffeeRoaster.map((roaster) => (
               <option key={roaster} value={roaster}>
                 {roaster}
@@ -90,11 +119,11 @@ const navigate = useNavigate();
             ))}
           </select>
           <label htmlFor="description">Description: </label>
-          <input type="text" name="description" />
+          <input type="text" name="description" defaultValue={singleCoffee.description} />
           <label htmlFor="price">Price: </label>
-          <input type="number" name="price" step="0.01" defaultValue={0} />
+          <input type="number" name="price" step="0.01" defaultValue={singleCoffee.price} />
           <label htmlFor="region">Region: </label>
-          <select name="region" required>
+          <select name="region" required defaultValue={singleCoffee.region}>
             <option value="">Select Coffee Region</option>
             {region.map((place) => (
               <option key={place} value={place}>
@@ -103,10 +132,10 @@ const navigate = useNavigate();
             ))}
           </select>
           <label htmlFor="weight">Weight(oz): </label>
-          <input type="number" name="weight" defaultValue={0} />
+          <input type="number" name="weight" defaultValue={singleCoffee.weight} />
 
           <label htmlFor="flavor_profile">Flavor: </label>
-          <select name="flavor_profile" required>
+          <select name="flavor_profile" required defaultValue={singleCoffee.flavor_profile}>
             <option value="">Select Flavor</option>
             {flavor_profile.map((flavor) => (
               <option key={flavor} value={flavor}>
@@ -116,7 +145,7 @@ const navigate = useNavigate();
           </select>
 
           <label htmlFor="roast">Roast: </label>
-          <select name="roast" required>
+          <select name="roast" required defaultValue={singleCoffee.roast}>
             <option value="">Select Roast</option>
             {roast.map((r) => (
               <option key={r} value={r}>
@@ -126,7 +155,7 @@ const navigate = useNavigate();
           </select>
 
           <label htmlFor="rating">Rating: </label>
-          <select name="rating" required>
+          <select name="rating" required defaultValue={singleCoffee.rating}>
             <option value="">Select Rating</option>
             {rating.map((rate) => (
               <option key={rate} value={rate}>
