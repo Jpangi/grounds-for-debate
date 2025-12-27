b89bf4e321e34885532aad4b47da59bb4d79a62a
jpangi <jacob.Pangilinan10@gmail.com>
2025-12-26 16:17:09 -0800

    udpated cors url

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://grounds-for-debate.onrender.com/", // exact frontend URL
+    origin: "https://grounds-for-debate.onrender.com", // exact frontend URL
     credentials: true,
   })
 );
